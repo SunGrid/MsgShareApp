1b191786eaa06f6cdf7ad89e0b4a7a6d222e986b
Michael <mlonnecker@gmail.com>
2019-05-31 15:17:27 -0600

    Section Two, Video Three

--- app/src/main/java/com/vivospice/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/vivospice/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vivospice.msgshareapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -15,6 +16,17 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener{
             Log.i("MainActivity", "Button was clicked !")
             Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+
+            val message: String =etUserMessage.text.toString()
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
---
+++ app/src/main/java/com/vivospice/msgshareapp/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.vivospice.msgshareapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.vivospice.msgshareapp.R.layout.activity_second
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_second)
+    }
+}
