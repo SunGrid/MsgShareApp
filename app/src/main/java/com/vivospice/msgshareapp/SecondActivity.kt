152f34c741e42120178f79d24ac668a2c08c280a
Michael <mlonnecker@gmail.com>
2019-06-04 10:52:31 -0600

    Section 2.5 video 11

--- app/src/main/java/com/vivospice/msgshareapp/SecondActivity.kt
+++ app/src/main/java/com/vivospice/msgshareapp/SecondActivity.kt
@@ -2,11 +2,21 @@ package com.vivospice.msgshareapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.vivospice.msgshareapp.R.layout.activity_second
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_second)
+
+         val bundle: Bundle? =  intent.extras
+        val msg = bundle!!.getString("user_message")
+
+        Toast.makeText(this, msg , Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text = msg
+
     }
 }
