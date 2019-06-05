6be53cad3f5e4d2340bde9f6e6303338b5715e5a
Michael <mlonnecker@gmail.com>
2019-06-05 17:00:21 -0600

    Video 14 Created model with Kotlin data class

---
+++ app/src/main/java/com/vivospice/msgshareapp/Model.kt
@@ -0,0 +1,40 @@
+package com.vivospice.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier { // object is like static in java
+
+    val hobbies = listOf<Hobby>(
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Sleeping"),
+        Hobby("Gaming"),
+        Hobby("Programming"),
+        Hobby("Talking")
+    )
+
+}
