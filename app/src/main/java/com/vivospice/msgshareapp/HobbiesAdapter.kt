cfcfa83987c630f8fbc0dbf1cfa20bb6308fee59
Michael <mlonnecker@gmail.com>
2019-06-06 22:03:41 -0600

    Video 16 https://youtu.be/HXz6618Zzn8?list=PLlxmoA0rQ-Lw5k_QCqVl3rsoJOnb_00UV&t=475

--- app/src/main/java/com/vivospice/msgshareapp/HobbiesAdapter.kt
+++ app/src/main/java/com/vivospice/msgshareapp/HobbiesAdapter.kt
@@ -7,34 +7,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) : RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
+class HobbiesAdapter(val context: Context, val hobbies: List<Hobby>) :
+    RecyclerView.Adapter<HobbiesAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-val view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
-   return MyViewHolder(view)
+        val view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(view)                               //
     }
 
-    override fun getItemCount(): Int {  //length of hobbies List
+    override fun getItemCount(): Int {                  //length of hobbies List
         return hobbies.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-val hobby = hobbies[position]
+        val hobby = hobbies[position]
         holder.setData(hobby, position)
     }
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var currentHobby: Hobby? = null
         var currentPosition: Int = 0
 
-        init{
+        init {
             itemView.setOnClickListener {
                 Toast.makeText(context, currentHobby!!.title + "Clicked !", Toast.LENGTH_SHORT).show()
-
             }
 
             itemView.imgShare.setOnClickListener {
@@ -45,13 +44,15 @@ val hobby = hobbies[position]
                 intent.type = "text/plain"
 
                 context.startActivity(Intent.createChooser(intent, "Share to : "))
-                 //startActivity is the method of context so must put context word before it
+                                                     //startActivity is the method of context so must put context word before it
             }
         }
 
 
-        fun setData(hobby: Hobby?, pos: Int){  // Hobby? Allows this reference to hold null eliminating NullPointerExceptions from code
-            itemView.txvTitle.text = hobby!!.title
+        fun setData(hobby: Hobby?, pos: Int) {
+                                                    // Hobby? Allows this reference to hold null eliminating NullPointerExceptions from code
+            itemView.txvTitle.text = hobby!!.title  //hobby!! if null will cause NPE
+                                                    //title is from Model.kt, data class Hobby(var title: String)
 
             this.currentHobby = hobby
             this.currentPosition = pos
